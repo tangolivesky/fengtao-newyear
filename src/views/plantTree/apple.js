0d333af12d56db8d65518455a6ecb2cb2ada3628
tonytang <tangolivesky@163.com>
2022-03-16 20:45:14 +0800

    替换苹果

--- src/views/plantTree/apple.js
+++ src/views/plantTree/apple.js
@@ -5,24 +5,48 @@ const stars = [
         status: false,
         x: 350,
         y: 0,
-        width: 32,
-        height:40
+        width: 35,
+        height:35
     },
     {
         id: 2,
         status: false,
         x: 320,
         y: 40,
-        width: 32,
-        height:40
+        width: 35,
+        height:35
     },
     {
         id: 3,
         status: false,
-        x: 20,
-        y: 280,
-        width: 20,
-        height: 20,
+        x: 380,
+        y: 38,
+        width: 36,
+        height:36
+    },
+    {
+        id: 4,
+        status: false,
+        x: 290,
+        y: 80,
+        width: 36,
+        height:36
+    },
+    {
+        id: 5,
+        status: false,
+        x: 340,
+        y: 80,
+        width: 36,
+        height:36
+    },
+    {
+        id: 6,
+        status: false,
+        x: 400,
+        y: 80,
+        width: 36,
+        height:36
     },
     
 ]
